8ef27b9ce08a4a76e10f87105d1253ee5e191c50
Benja Share <benja1006@gmail.com>
2023-04-25 15:13:37 -0700

    switched to argparse

--- server/server.py
+++ server/server.py
@@ -3,6 +3,7 @@ import socket
 import selectors
 import traceback
 import asyncio
+import argparse
 
 # joycontrol imports
 from joycontrol.memory import FlashMemory
@@ -11,6 +12,8 @@ from joycontrol import logging_default as log, utils
 from joycontrol.protocol import controller_protocol_factory
 from joycontrol.server import create_hid_server
 from joycontrol.command_line_interface import ControllerCLI
+
+
 
 sel = selectors.DefaultSelector()
 
@@ -23,19 +26,18 @@ def accept_wrapper(sock):
     message = serverlib.Message(sel, conn, addr)
     sel.register(conn, selectors.EVENT_READ, data=message)
 
-if len(sys.argv) != 3:
-    print(f"Usage: {sys.argv[0]} <> <port>")
-    sys.exit(1)
 
 
-host, port = 'localhost', 12345
-lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-lsock.bind((host, port))
-lsock.listen()
-print(f"listening on {(host, port)}")
-lsock.setblocking(False)
-sel.register(lsock, selectors.EVENT_READ, data=None)
+def setup_server(args):
+    host = 'localhost'
+    port = args.port
+    lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    lsock.bind((host, port))
+    lsock.listen()
+    print(f"listening on {(host, port)}")
+    lsock.setblocking(False)
+    sel.register(lsock, selectors.EVENT_READ, data=None)
 
 # Controller specific things
 spi_flash = FlashMemory()
@@ -50,12 +52,12 @@ class Emulator:
         print("Got Controller")
         with utils.get_output(path='log.txt', default=None) as capture_file:
             # prepare the the emulated controller
-            factory = controller_protocol_factory(controller, spi_flash=spi_flash, reconnect='')
+            factory = controller_protocol_factory(controller, spi_flash=spi_flash, reconnect=args.reconnect_bt_addr)
             ctl_psm, itr_psm = 17, 19
-            transport, protocol = await create_hid_server(factory, reconnect_bt_addr='',
+            transport, protocol = await create_hid_server(factory, reconnect_bt_addr=args.reconnect_bt_addr,
                                                         ctl_psm=ctl_psm,
                                                         itr_psm=itr_psm, capture_file=capture_file,
-                                                        device_id='')
+                                                        device_id=args.device_id)
             print("Made hid server")
             self.controller_state = protocol.get_controller_state()
             # self.controller_state.connect()
@@ -91,34 +93,23 @@ class Emulator:
     
 
     
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('controller', help='JOYCON_R, JOYCON_L or PRO_CONTROLLER')
+    parser.add_argument('-l', '--log', help="BT-communication logfile output")
+    parser.add_argument('-d', '--device_id', help='not fully working yet, the BT-adapter to use')
+    parser.add_argument('--spi_flash', help="controller SPI-memory dump to use")
+    parser.add_argument('-r', '--reconnect_bt_addr', type=str, default=None,
+                        help='The Switch console Bluetooth address (or "auto" for automatic detection), for reconnecting as an already paired controller.')
+    parser.add_argument('--nfc', type=str, default=None, help="amiibo dump placed on the controller. Äquivalent to the nfc command.")
+    parser.add_argument('-p', '--port', type=int, default=12345, help="The port to run the server on.")
+    args = parser.parse_args()
+    
+    setup_server(args)
+    emulator = Emulator()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(
+        emulator.setup()
+    )
 
-emulator = Emulator()
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(
-    emulator.setup()
-)
-
-
-# server stuff?
-# try: 
-#     while True:
-#         events = sel.select(timeout=None)
-#         for key, mask in events:
-#             if key.data is None:
-#                 accept_wrapper(key.fileobj)
-#             else:
-#                 message = key.data
-#                 try:
-#                     message.process_events(mask, emulator)
-#                 except Exception:
-#                     print(
-#                         f" Main: Error: Exception for {message.addr}:\n"
-#                         f"{traceback.format_exc()}"
-#                     )
-#                     message.close()
-
-# except KeyboardInterrupt:
-#     print("Caught keyboard interrupt, exiting")
-# finally:
-#     sel.close()
