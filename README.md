d57368911dd5a37709b63b47b7fb517278d30189
benja <benja1006@gmail.com>
2023-04-30 02:31:54 -0700

    Need to test trading

--- README.md
+++ README.md
@@ -4,8 +4,7 @@ https://github.com/mart1nro/joycontrol
 
 
 TODO:
-~make functions actually interact with the switch~
+
 
 maybe:
-fix file writing errors? not sure what these are from / why they happen
-~might have to make sure that joycontrol keeps pinging the switch with no-ops~
+fix file writing errors? not sure what these are from / why they happen - not sure if this still happens 
--- client/Game.py
+++ client/Game.py
@@ -3,6 +3,7 @@ import numpy as np
 import asyncio
 from client import main
 import pytesseract
+import argparse
 
 class Game:
     def __init__(self):
@@ -13,6 +14,8 @@ class Game:
         self.game_state = ''
         self.frame = None
         self.trade_state = 'Looking for trade'
+        self.names = None
+        self.trades_done = 0
         
 
     def update_game_state(self):
@@ -48,49 +51,113 @@ class Game:
             if cv.waitKey() == ord('q'):
                 exit()
         
-    def trade_loop(self):
+    async def trade_loop(self):
         while True:
             ret, self.frame = self.cap.read()
             self.frame = cv.cvtColor(self.frame, cv.COLOR_BGR2GRAY)
             if not ret:
                 print("Can't receive frame")
                 exit()
-            
+            text_frame = self.frame[820:940, 530:1300]
+        
+            text = [*pytesseract.image_to_string(text_frame).replace('\n', '')]
+            print('Text frame:', text)
             # cv.imshow('Frame', self.frame)
+            await self.check_disconnect()
+
             if self.trade_state == 'Look for trade':
                 self.start_trade()
             if self.trade_state == 'Looking for trade':
                 self.check_trade_found()
             if self.trade_state == 'Trade found':
-                self.check_name()
-            if self.trade_state == 'Waiting for accept':
-                pass
+                await self.check_name()
+            if self.trade_state == 'Waiting for other':
+                await self.finish_trade()
+            if self.trade_state == 'Waiting for trade to finish':
+                await self.check_trade_done()
             if cv.waitKey() == ord('q'):
                 exit()
     
-    def check_name(self):
+    async def check_name(self):
         name_frame = self.frame[10:70, 400:750]
-        name = pytesseract.image_to_string(name_frame).replace('\n', '')
+        name = pytesseract.image_to_string(name_frame).replace('\n', '').lower()
         if name in self.names:
-            self.complete_trade()
+            self.trade_partner = name
+            await self.innitiate_trade()
         else:
-            self.exit_trade()
+            await self.exit_trade()
     
-    def check_trade_found(self):
+    async def check_trade_found(self):
         text_frame = self.frame[820:940, 530:1300]
         
         text = pytesseract.image_to_string(text_frame).replace('\n', '')
         if text == 'A trade partner has been found!':
             self.trade_state = 'Trade found'
+        if 'No one could be found!' in text:
+            await tap('b')
+            self.trade_state = 'Look for trade'
 
-    def complete_trade(self):
-        # inputs to complete trade, need to wait for them to accept
-        # update an internal counter of how many trades have been completed
-        pass
+    async def innitiate_trade(self):
+        if self.trades_done > 0:
+            await l_stick_flick(RIGHT)
+            await asyncio.sleep(0.5)
+        await tap('a')
+        await asyncio.sleep(0.5)
+        await tap('a')
+        self.trade_state = 'Waiting for other'
+
+    async def finish_trade(self):
+        text_frame = self.frame[820:940, 530:1300]
+        text = pytesseract.image_to_string(text_frame).replace('\n', '')
+        if "Do you want to trade" in text:
+            await tap('a')
+            await asyncio.sleep(0.5)
+            await tap('a')
+        self.trades_done  += 1
+        self.names.remove(self.trade_partner)
+        if self.names:
+            self.start_trade()
+        
     
-    def exit_trade(self):
+    async def exit_trade(self):
         # quit trade inputs
+        await tap('b')
+        await asyncio.sleep(0.5)
+        await tap('a')
+        await asyncio.sleep(2.0)
         self.trade_state = 'Look for trade'
+
+    async def start_trade(self, names = None):
+        if not names and not self.names:
+            print('Please enter at least one name')
+        self.names += names
+        self.trade_partner = ''
+        await tap('a')
+        await asyncio.sleep(0.5)
+        await tap('a')
+        await asyncio.sleep(0.5)
+        await tap('a')
+        await asyncio.sleep(0.5)
+        self.trade_state = 'Looking for trade'
+        await self.trade_loop()
+
+    async def check_disconnect(self):
+        text_frame = self.frame[820:940, 530:1300]
+        text = pytesseract.image_to_string(text_frame).replace('\n', '')
+
+        if "Your trading partner chose to quit trading" in text:
+            await tap('b')
+            await asyncio.sleep(0.5)
+            await tap('b')
+            self.trade_state = 'Look for trade'
+
+    async def check_trade_done(self):
+        text_frame = self.frame[820:940, 530:1300]
+        text = pytesseract.image_to_string(text_frame).replace('\n', '')
+        if 'Take good care of' in text:
+            await tap('b')
+            await asyncio.sleep(0.5)
+            await self.exit_trade()
 
 # self.frame = cv.rectangle(self.frame, (400,10), (750,70), (255, 0, 0), 2) # name bounding box
 # self.frame = cv.rectangle(self.frame, (478,780), (1440,980), (255, 0, 0), 2) # bottom bounding box
@@ -136,4 +203,14 @@ DOWNRIGHT=(4095, 0)
 
 if __name__ == "__main__":
     game = Game()
-    game.trade_loop()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--mode', help='The mode to run in', choices=['trade',  'hatch'])
+    parser.add_argument('-n', '--names', help='Names of users', nargs='+', required=False)
+
+    args = parser.parse_args()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(
+        game.start_trade(args.names)
+    )
+    
